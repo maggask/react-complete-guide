7f2a1dd626208aaf29ebafc703b9933479ce297b
Margret S. Kristjansdottir <maggask@gmail.com>
2021-09-18 16:56:29 +0000

    Working on events

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,20 +1,31 @@
+import React, { useState } from 'react';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import Card from '../UI/Card';
+import ExpensesFilter from './ExpensesFilter';
 
 const Expenses = ({items}) => {
+    const [expensesFilter, setExpensesFilter] = useState('');
+
+    const selectExpenseFilter = (selectedFilter) => {
+        setExpensesFilter(selectedFilter);
+    }
+
     return (
-        <Card className='expenses'>
-            {items.map((item, index) => {
-                return (
-                    <ExpenseItem 
-                    key={item + index}
-                    title={item.title} 
-                    amount={item.amount} 
-                    date={item.date}/>
-                );
-            })}
-        </Card>
+        <div>
+            <ExpensesFilter selected={expensesFilter} onSelectExpensesFilter={selectExpenseFilter}/>
+            <Card className='expenses'>
+                {items.map((item, index) => {
+                    return (
+                        <ExpenseItem 
+                        key={item + index}
+                        title={item.title} 
+                        amount={item.amount} 
+                        date={item.date}/>
+                    );
+                })}
+            </Card>
+        </div>
     )
 }
 
