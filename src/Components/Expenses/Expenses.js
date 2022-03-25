c2248d195ff99aad384a821518dc7bdf0b0b3902
Margrét S. Kristjánsdóttir <margret@controlant.com>
2022-03-25 20:12:32 +0000

    Stuff

--- src/Components/Expenses/Expenses.js
+++ src/Components/Expenses/Expenses.js
@@ -1,29 +1,36 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 
-const Expenses = ({items}) => {
+const Expenses = ({expenses}) => {
     const [expensesFilter, setExpensesFilter] = useState('');
+    const [filteredExpenses, setFilteredExpenses] = useState([...expenses]);
 
     const selectExpenseFilter = (selectedFilter) => {
         setExpensesFilter(selectedFilter);
     }
 
+    useEffect(() => {
+        setFilteredExpenses(expenses.filter((expense) => expense.date.getFullYear().toString() === expensesFilter));
+    }, [expenses, expensesFilter])
+
     return (
         <div>
             <ExpensesFilter selected={expensesFilter} onSelectExpensesFilter={selectExpenseFilter}/>
             <Card className='expenses'>
-                {items.map((item, index) => {
+                {filteredExpenses.length === 0 ? (
+                    <p>No expenses found!</p>
+                ) : (filteredExpenses.map((expense) => {
                     return (
                         <ExpenseItem 
-                        key={item + index}
-                        title={item.title} 
-                        amount={item.amount} 
-                        date={item.date}/>
+                            key={expense.id}
+                            title={expense.title} 
+                            amount={expense.amount} 
+                            date={expense.date}/>
                     );
-                })}
+                }))}
             </Card>
         </div>
     )
