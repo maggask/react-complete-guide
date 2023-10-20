fd457e565dc172333277c9b58c8f5a0e22038962
Margrét S. Kristjánsdóttir <margret@controlant.com>
2023-10-20 11:17:20 +0000

    Project done

--- src/Components/Expenses/ExpensesList.js
+++ src/Components/Expenses/ExpensesList.js
@@ -3,6 +3,10 @@ import ExpenseItem from "./ExpenseItem";
 import "./ExpensesList.css";
 
 const ExpensesList = ({ items }) => {
+  if (items.length === 0) {
+    return <h2 className="expenses-list__fallback">Found no expense</h2>;
+  }
+
   return (
     <ul className="expenses-list">
       {items.map((item) => (
